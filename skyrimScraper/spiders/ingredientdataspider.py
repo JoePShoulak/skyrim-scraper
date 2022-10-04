7c947ece622b0500fb51e42a34d16d07ce0d9c63
Joe P Shoulak <joe@squarecompass.io>
2022-10-04 18:46:32 -0500

    comprehensive ingredient data from available source

--- skyrimScraper/spiders/ingredientdataspider.py
+++ skyrimScraper/spiders/ingredientdataspider.py
@@ -1,3 +1,4 @@
+from operator import truediv
 import scrapy, json, os
 
 def getIngredientURLS():
@@ -8,6 +9,28 @@ def getIngredientURLS():
 
     return urls
 
+def checkEffectLink(link):
+    if ".png" in link or "Form_ID" in link:
+        return False
+    if "Hearthfire" in link or "Dawnguard" in link or "Dragonborn" in link:
+        return False
+    if "Alchemy" in link:
+        return False
+
+    return True
+
+# Quick and dirty way to grab an attr from a DOM element
+def getAttr(element, attr):
+    return element.split(attr + "=")[1].split(" ")[0]
+
+
+# Because these lazy programmers didn't include proper classing in their HTML, we have to do some gross parsing
+def parsedUrl(dataRow, index=1):
+    link = dataRow.css('a').getall()[index]
+    url = getAttr(link, "href").replace('"', '')
+
+    return "https://en.uesp.net" + url
+
 class IngredientDataSpider(scrapy.Spider):
     name = 'ingredient_data'
     start_urls = getIngredientURLS()
@@ -15,26 +38,35 @@ class IngredientDataSpider(scrapy.Spider):
     def parse(self, response):
         table = response.css('.infobox')
 
-        name = table.css('a').attrib['title']
-
         data = table.css('td::text').getall()
-        value = data[0]
-        weight = data[1]
 
-        links = table.css('a::text').getall()
-        print(links)
-        effects = links[1:]
-        if len(effects) > 4:
-            effects = effects[:-1]
+        effects = table.css('a::text').getall()
 
+        links = table.css('a')
+
+        # print(links)
+
+        parsedlinks = []
+
+        for i in range(len(links)):
+            parsedlinks += [parsedUrl(links, i)]
+
+        parsedlinks = list(filter(checkEffectLink, parsedlinks))[:8]
+        
+        effects = effects[1:5]
 
         yield {
-            "name": name,
-            "value": value,
-            "weight": weight,
+            "name": table.css('a').attrib['title'],
+            "value": data[0],
+            "weight": data[1],
 
             "effect1_name": effects[0],
             "effect2_name": effects[1],
             "effect3_name": effects[2],
             "effect4_name": effects[3],
+
+            "effect1_url": parsedlinks[0],
+            "effect2_url": parsedlinks[2],
+            "effect3_url": parsedlinks[4],
+            "effect4_url": parsedlinks[6],
         }
